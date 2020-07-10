81648d202610a1ce8e627cb98f3319ee9c54b572
EDnotSheeran <juninho.tab3@gmail.com>
2020-07-10 09:21:04 -0300

    add files

---
+++ src/App.js
@@ -0,0 +1,33 @@
+/* App.js */
+
+import React, { Fragment } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Helmet } from "react-helmet";
+
+import Posts from "./Posts";
+import Post from "./Post";
+import NotFound from "./NotFound";
+import data from "./data";
+
+export default () => (
+  <Fragment>
+    <Helmet titleTemplate="%s | My Blog" />
+    
+        <Router>
+          <Switch>
+            <Route exact path="/" render={routeProps => <Posts {...data} />} />
+
+            {Object.entries(data.posts).map(([slug, post]) => (
+              <Route 
+                key={slug}
+                exact
+                path={`/${slug}`}
+                render={({ match }) => <Post {...post} />}
+              />
+            ))}
+
+            <Route render={routeProps => <NotFound />} />
+          </Switch>
+        </Router>
+  </Fragment>
+);
