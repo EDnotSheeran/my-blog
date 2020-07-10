794d21af9466b1f9dbcf7e63db01e6a241a5e0af
EDnotSheeran <juninho.tab3@gmail.com>
2020-07-10 10:12:48 -0300

    Updates

---
+++ precache-manifest.34b8b2e1280080bb7f5dec801ab1919d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f0ba6dbded8e2f0df9eb077d66609d2",
+    "url": "/my-blog/index.html"
+  },
+  {
+    "revision": "3b5e4a4c75454d2f4308",
+    "url": "/my-blog/static/css/2.1bafa09e.chunk.css"
+  },
+  {
+    "revision": "304a0114d909b01067de",
+    "url": "/my-blog/static/css/main.cdc07771.chunk.css"
+  },
+  {
+    "revision": "3b5e4a4c75454d2f4308",
+    "url": "/my-blog/static/js/2.e9d66c25.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-blog/static/js/2.e9d66c25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "304a0114d909b01067de",
+    "url": "/my-blog/static/js/main.eecf88cc.chunk.js"
+  },
+  {
+    "revision": "36686c32f1b2062287aa",
+    "url": "/my-blog/static/js/runtime-main.10735d37.js"
+  }
+]);
